a5b4a4cfd1e54d931102fd9598e321157c5940ef
xloc <tj-xzx@126.com>
2017-04-16 00:38:04 +0800

    实现了更智能的更新策略，只输出继上次取值之后的更新的变量

--- storage.py
+++ storage.py
@@ -1,14 +1,26 @@
+import time
+
 
 class VariableTable(object):
     def __init__(self):
         self.value = {}
+        
+        self.update_time = {}
+        self.last_extract_time = 0
 
     def update(self, name, value):
         # type: (str, object) -> None
         self.value[name] = value
+        self.update_time[name] = time.time()
 
     def extract(self):
         # type: () -> dict
-        return self.value.copy()
+        result = {
+            k: self.value[k]
+            for k, updated_time in self.update_time.iteritems()
+            if updated_time > self.last_extract_time
+        }
+        self.last_extract_time = time.time()
+        return result
 
 table = VariableTable()
