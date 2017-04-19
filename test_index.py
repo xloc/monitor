b1f3e05aa9cf9614672e4652a949e11cb7b55223
xloc <tj-xzx@126.com>
2017-04-19 21:15:02 +0800

    Fit PEP8 Format

--- test_index.py
+++ test_index.py
@@ -6,9 +6,9 @@ from monitor_server import app
 from storage import table
 
 if __name__ == '__main__': 
-    #var = itertools.count(0)
+    # var = itertools.count(0)
     def update_thread():
-        var = itertools.cycle(range(0,100))
+        var = itertools.cycle(range(0, 100))
         while True:
             table.update(a=var.next())
             time.sleep(1)
