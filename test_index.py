b11d0c18fc5efdf27400738eca40f81871506007
cx-z <1612379420@qq.com>
2017-04-19 20:59:19 +0800

    test_index.py
    将产生数据的var = itertools.cycle(range(0,100))放进了update_thread函数里面。然后给数据更新加了一个循环

--- test_index.py
+++ test_index.py
@@ -5,12 +5,13 @@ import time
 from monitor_server import app
 from storage import table
 
-if __name__ == '__main__':
-    var = itertools.cycle(range(0, 100))
-
+if __name__ == '__main__': 
+    #var = itertools.count(0)
     def update_thread():
-        table.update(a=var.next())
-        time.sleep(1)
+        var = itertools.cycle(range(0,100))
+        while True:
+            table.update(a=var.next())
+            time.sleep(1)
     t = threading.Thread(target=update_thread)
     t.setDaemon(True)
     t.start()
