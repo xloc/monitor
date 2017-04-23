f618e946bf0b7eba0b2a327a61f8f95da7046daf
xloc <tj-xzx@126.com>
2017-04-23 20:31:21 +0800

    增加图片数据传输

--- storage.py
+++ storage.py
@@ -4,23 +4,32 @@ import time
 class VariableTable(object):
     def __init__(self):
         self.value = {}
+        self.format = {}
         
         self.update_time = {}
         self.last_extract_time = 0
 
+    def add_var(self, name, fmt):
+        self.format[name] = fmt
+
     def update(self, **kwargs):
         now = time.time()
         for name, value in kwargs.iteritems():
-            self.value[name] = value
-            self.update_time[name] = now
+            if name in self.format.keys():
+                self.value[name] = value
+                self.update_time[name] = now
+            else:
+                raise ValueError('Update non-exist variable')
 
     def extract(self):
         # type: () -> dict
         result = {
-            k: self.value[k]
+            k: {'format': self.format[k], 'value': self.value[k]}
             for k, updated_time in self.update_time.iteritems()
             if updated_time > self.last_extract_time
         }
+        result = {'vars': result}
+
         self.last_extract_time = time.time()
         return result
 
--- test_index.py
+++ test_index.py
@@ -1,18 +1,37 @@
 import itertools
+import json
 import threading
 import time
 
 from monitor_server import app
 from storage import table
 
-if __name__ == '__main__': 
-    # var = itertools.count(0)
-    def update_thread():
+if __name__ == '__main__':
+
+    table.add_var('a', 'plain')
+
+    def int_update_thread():
         var = itertools.cycle(range(0, 100))
         while True:
             table.update(a=var.next())
             time.sleep(1)
-    t = threading.Thread(target=update_thread)
+
+    t = threading.Thread(target=int_update_thread)
+    t.setDaemon(True)
+    t.start()
+
+    table.add_var('a', 'img')
+
+    imgs = None
+    with open('b64_imgs') as f:
+        imgs = json.load(f)
+
+    def img_update_thread():
+        var = itertools.cycle(imgs.itervalues())
+        while True:
+            table.update(image=var.next())
+            time.sleep(1)
+    tg = threading.Thread(target=img_update_thread)
     t.setDaemon(True)
     t.start()
 
