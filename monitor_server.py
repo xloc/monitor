f2cf04bdee7e8cb6efdf5991485090d2e6d09b3d
xloc <tj-xzx@126.com>
2017-04-26 17:46:41 +0800

    Refactor web server to adapt model changes

--- monitor_server.py
+++ monitor_server.py
@@ -14,7 +14,7 @@ def index():
 @app.route('/get_vars', methods=['GET', 'POST'])
 def get_vars():
     print 'called'
-    return jsonify(table.extract())
+    return jsonify(table.extract_all())
 
 if __name__ == '__main__':
     app.run()
--- test_index.py
+++ test_index.py
@@ -8,19 +8,19 @@ from storage import table
 
 if __name__ == '__main__':
 
-    table.add_var('a', 'plain')
+    # table.add_var('a', 'plain')
 
     def int_update_thread():
         var = itertools.cycle(range(0, 100))
         while True:
-            table.update(a=var.next())
+            table.a=var.next()
             time.sleep(1)
 
     t = threading.Thread(target=int_update_thread)
     t.setDaemon(True)
     t.start()
 
-    table.add_var('image', 'img')
+    # table.add_var('image', 'img')
 
     imgs = None
     with open('b64_imgs.json') as f:
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     def img_update_thread():
         var = itertools.cycle(imgs.itervalues())
         while True:
-            table.update(image=var.next())
+            table.img=var.next()
             time.sleep(1)
     tg = threading.Thread(target=img_update_thread)
     tg.setDaemon(True)
