7fb43c336ad3ffd7dbead9ca9786566dba64e1be
xloc <tj-xzx@126.com>
2017-05-01 15:08:31 +0800

    Finish handle var update

--- monitor_server.py
+++ monitor_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.templating import render_template
 
 from storage import table
@@ -14,6 +14,12 @@ def index():
 @app.route('/get_vars', methods=['GET', 'POST'])
 def get_vars():
     return jsonify(table.get_supervised())
+
+
+@app.route('/set_var')
+def set_var():
+    print request.args
+    return jsonify(dict(update='OK'))
 
 if __name__ == '__main__':
     app.run()
